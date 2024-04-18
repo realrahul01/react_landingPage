cb658baa8a000c4875c4d98c1576405fa523d12c
realrahul01 <dev.ecomed@gmail.com>
2024-04-18 20:49:42 +0530

    initial commit

---
+++ src/component/Card.jsx
@@ -0,0 +1,17 @@
+
+const Card = ({image, title, description1, description2})=>{
+    return(
+        <div className="card_work">
+            <img src={image} alt="error" />
+            <div className="content">
+            <h1>{title}</h1>
+            <p className='val1'>{description1}</p>
+            <p className='val2'>{description2}</p>
+            </div>
+        </div>
+    )
+
+    // fjnwelnlwenvew
+}
+
+export default Card;
